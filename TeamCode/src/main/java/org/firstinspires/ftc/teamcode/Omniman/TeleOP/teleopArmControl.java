a096e7f3c9761fca6d8d9ab6028467902e2ca117
noahl <rambots@weberschool.org>
2024-11-17 12:55:17 -0500

    Bruh

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Omniman/TeleOP/teleopArmControl.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.Omniman.TeleOP;
+
+import com.acmerobotics.roadrunner.Pose2d;
+
+import org.firstinspires.ftc.teamcode.Drive.MecanumDrive;
+import org.firstinspires.ftc.teamcode.Omniman.Omniman;
+public class teleopArmControl {
+}
